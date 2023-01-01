6757d91bb108cd5bd6bac7d68028349f711c9372
Sanjay Govind <sanjay.govind9@gmail.com>
2023-01-01 22:22:34 +1300

    Fix building issues and update deps

--- build.sh
+++ build.sh
@@ -3,5 +3,5 @@ do
     echo $value
     mkdir -p out/$value
     dotnet publish -c Release -r $value -p:PublishSingleFile=true -p:DebugType=None -p:DebugSymbols=false -p:IncludeNativeLibrariesForSelfExtract=true --self-contained true  
-    cp bin/Release/net6.0/$value/publish/* out/$value
+    cp bin/Release/net7.0/$value/publish/* out/$value
 done;
